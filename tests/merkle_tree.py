94f7a6ecdd640561fb228cf7cda272d46af39eba
almsh <shestakov.alex@gmail.com>
2022-02-11 19:52:34 +0300

    add verify_slice_proof_inplace

--- tests/merkle_tree.py
+++ tests/merkle_tree.py
@@ -3,6 +3,7 @@ from itertools import zip_longest
 from brownie import web3
 from eth_utils import encode_hex, to_bytes
 
+
 class MerkleTree:
     def __init__(self, elements):
         self.elements = [web3.keccak(hexstr=el) for el in elements]
@@ -100,12 +101,51 @@ class MerkleTree:
 
         return root == layer[0]
 
+    @staticmethod
+    def verify_slice_proof_inplace(root, proof, elements, start):
+        work_layer = [None] * (len(elements) + 2)
+        # init work layer
+        for i in range(0, len(elements)):
+            work_layer[i + 1] = web3.keccak(hexstr=elements[i])
+
+        start_tree_index = start
+        end_tree_index = start + len(elements) - 1
+        proof_index = 0
+        range_end = len(elements) + 1
+
+        while proof_index < len(proof) or range_end > 2:
+            start_offset = 0
+            if start_tree_index % 2 == 1:
+                work_layer[0] = to_bytes(hexstr=proof[proof_index])
+                proof_index += 1
+                start_offset = 1
+
+            if end_tree_index % 2 == 0:
+                work_layer[range_end] = to_bytes(hexstr=proof[proof_index])
+                range_end += 1
+                proof_index += 1
+
+            range_end = (range_end + start_offset) // 2 + 1
+            for i in range(1, range_end):
+                pair_first_index = 2 * i - start_offset - 1
+                pair_second_index = 2 * i - start_offset
+                work_layer[i] = MerkleTree.combined_hash(work_layer[pair_first_index], work_layer[pair_second_index])
+
+            start_tree_index //= 2
+            end_tree_index //= 2
+
+        return root == work_layer[1]
+
 
 if __name__ == "__main__":
-
-    text_leafs = list(range(0,32))
+    text_leafs = list(range(0, 32))
     leafs = MerkleTree.sort_elements([encode_hex(str(el)) for el in text_leafs])
     tree = MerkleTree(leafs)
-    proof = tree.get_slice_proof(0,1)
-    print(proof)
-    print(MerkleTree.verify_slice_proof(tree.root, proof, leafs[0:1], 0))
+
+    for start in range(0, 33):
+        for end in range(start + 1, 33):
+            proof = tree.get_slice_proof(start, end)
+            leafs_to_proof = leafs[start:end]
+            simple_proof = MerkleTree.verify_slice_proof(tree.root, proof, leafs_to_proof, start)
+            inplace_proof = MerkleTree.verify_slice_proof_inplace(tree.root, proof, leafs_to_proof, start)
+            print(start, end, len(proof), inplace_proof)
